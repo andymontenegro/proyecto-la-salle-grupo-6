9bc3621245af89e80c7f9e0aee04aeb0b293e3b0
EricDevs <ing.sotillo@gmail.com>
2023-05-27 12:42:43 +0200

    VerEric

--- entregable_grupo_6_responsive/js/cats.js
+++ entregable_grupo_6_responsive/js/cats.js
@@ -1,6 +1,52 @@
 console.log('Javascript cargado');
 const url = 'https://catfact.ninja/fact';
 const div = document.querySelector('#cats-facts');
+const span = "<spam></spam>"
+//const fact = div.querySelector("#article");
+const observer = new IntersectionObserver( entries => {
+//cuando esta intersectando debo solicitar un nuevo fact
+//cuando ha dejado de intersectar puedo borrar el primero
+//console.log(entries);
+
+/*
+    | PANTALLA     | <Article<span>> 
+
+*/ 
+    let entry = entries[0];
+    if (entry.isIntersecting){
+        
+        if (!entry.target.nextElementSibling) {
+            
+                showFact();
+            
+            
+            //loadFact();
+            console.log("Es el ultimo - Agregar")
+        }
+        
+        console.log("Elemento Visible");
+    } else {
+        if (!entry.target.previousElementSibling){
+            //cleanupOne();
+            //entry.target.remove
+            console.log("Es el primero - Quitar del dom y quitar obserser");
+            
+            div.removeChild(div.firstChild);
+            //observer.unobserve(entry.target);
+        } 
+        //console.log("No Visible");
+    }
+
+
+
+
+
+});
+
+
+
+
+//observer.observe(div);
 
 async function loadFact() {
     const resp = await fetch(url);
@@ -10,14 +56,20 @@ async function loadFact() {
 };
 
 function factArticle(fact) {
-    const article = document.createElement("article");
+    const article = document.createElement("article");  
     article.innerText = fact;
+    //<article>fact</article><span></span>|<article>fact</article><span></span><article>fact </article><span></span>|
     return article;
 }
+
+
 
 function renderFact(fact) {
     const article = factArticle(fact);
     div.appendChild(article);
+    const span = document.createElement("span");
+    article.appendChild(span);
+    observer.observe(span);
 }
 
 async function showFact() {
@@ -28,10 +80,31 @@ async function showFact() {
 async function cleanupOne() {
     div.removeChild(div.firstChild);
 }
-showFact();
-setInterval(showFact, 19000);
 
-setTimeout(() => setInterval(cleanupOne, 20000), 21000);
+
+/*
+
+cada elemento se anima por su cuenta
+haciendo de left 100% a left -300% 
+cuando desaparece la cola lo puedo quitar
+cuando aparece la cola es cuando debo cargar el siguiente
+
+
+<Elemento 0>
+<Elemento 2>(-522px)|<Elemento 3><Elemento 4><Elemento 5>
+Quitar elemento 2 y mover el contenedor a 0
+<Elemento 4>
+*/
+
+await showFact();
+
+
+//setInterval(showFact, 5000);
+
+//setTimeout(() => setInterval(cleanupOne, 20000), 21000);
+
+//observer.observe(div[0]);
+//console.log(fact);
 
 
 
---
+++ entregable_grupo_6_responsive/js/main.js
@@ -0,0 +1,14 @@
+/*let enlaces = document.querySelectorAll("a.nav-link");
+for (let link of enlaces){
+    document.addEventListener("keyup", function(event){
+        //console.log("event: " + event);
+        if ( link.dataset.key === event.key)  {
+            link.click();
+        }
+    })
+}*/
+
+import "./cats.js";
+import "./navKeys.js";
+
+
