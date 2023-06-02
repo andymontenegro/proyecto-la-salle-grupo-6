e29d7ec4a3aa710306d403dd07710d6925e442af
EricDevs <ing.sotillo@gmail.com>
2023-06-02 16:55:15 +0200

    Eric

--- entregable_grupo_6_responsive/js/navKeys.js
+++ entregable_grupo_6_responsive/js/navKeys.js
@@ -2,7 +2,19 @@ document.addEventListener("keyup", function(event){
     const maybeLink =  document.querySelector(`a.nav-link[data-key="${event.key}"]`);
      if (maybeLink) {maybeLink.click()};
  })
- 
+
+ //Detecto si presione P o N
+ document.addEventListener("keyup", function(event){
+  if (event.key == "p"){
+    console.log("Presiono P");
+    //moverse a la seccion previa
+
+  } else if (event.key == "n"){
+    console.log("Presiono N");
+    //Moverse a la siguiente seccion
+  }
+})
+
  
  document.querySelectorAll
  for (let input of document.querySelectorAll("input, textarea")){
@@ -19,12 +31,19 @@ document.addEventListener("keyup", function(event){
  };
  
  // MOSTRAR UN HELP apretando h
+ let statusModal = 0;
  document.addEventListener("keyup", function(event){
      const maybeLink =  event.key;
+     
      //console.log(maybeLink);
       if (maybeLink == "h") {
-         
+         if (statusModal == 0){
          showModal();
+         statusModal = 1;
+         } else {
+          closeModal();
+          statusModal= 0;
+         }
       };
   })
  
@@ -35,27 +54,59 @@ document.addEventListener("keyup", function(event){
      modal.style.paddingLeft = 0;
      modal.innerHTML= modalContent;
   }
+
+  let closeModal = function () {
+    let modal = document.querySelector(`[aria-label=navigationHelp]`);
+    modal.classList.remove('show');
+    modal.innerHTML = ""; 
+  }
   
  let modalContent = `<div class="modal-dialog">
  <div class="modal-content">
-   <div class="modal-header">
+   <div class="modal-header mx-auto">
      <h5 class="modal-title">Navegacion por Teclado</h5>
-     <p><b>Open / Close: </b> Press "h" </p>
    </div>
-   <div class="modal-body">
+   <div class="modal-body mx-auto">
    <h5 class="modal-title">Referencias:</h5><br>
-     <p><b>Seccion 1 - :</b> Press "1"</p>
-     <p><b>Seccion 2:</b> Press "2"</p>
-     <p><b>Seccion 3:</b> Press "3"</p>
-     <p><b>Seccion 4:</b> Press "4"</p>
-     <p><b>Seccion 5:</b> Press "5"</p>
-
-     <p><b>Seccion 5:</b> Press "5"</p>
-     <p><b>Seccion 5:</b> Press "5"</p>
-     <p><b>Seccion 5:</b> Press "5"</p>
+     <p><b>Open / Close Modal: </b> Press "H" </p>
+     <p>Press "1" to go <b>Seccion 1 - Who We Are?:</b></p>
+     <p>Press "2" to go <b>Seccion 2 - Services:</b></p>
+     <p>Press "3" to go <b>Seccion 3 - Portfolio:</b></p>
+     <p>Press "4" to go <b>Seccion 4 - Location:</b></p>
+     <p>Press "5" to go <b>Seccion 5 - Email Us:</b></p>
+     </div>
+     <div class="modal-body mx-auto">
+     <p><b>Next Seccion</b> Press "N"</p>
+     <p><b>Previous Seccion</b> Press "P"</p>
+     
    </div>
  </div>
  </div>`; 
 
 
  //viewport 
+var seccion1 = document.getElementById("we");
+var seccion2 = document.getElementById("services");
+var seccion3 = document.getElementById("portfolio");
+var seccion4 = document.getElementById("location");
+var seccion5 = document.getElementById("contact");
+
+var distance = seccion4.getBoundingClientRect();
+
+console.log("Distancia: " + distance.)
+
+
+function isInViewport(elem) {
+  var distance = elem.getBoundingClientRect();
+  return (
+      distance.top < (window.innerHeight || document.documentElement.clientHeight) && distance.bottom > 0
+  );
+}
+
+if (isInViewport(seccion1)) {
+  // está en el viewport
+  console.log("Seccion 1 en vierport");
+}
+
+
+ // https://desarrolloweb.com/faq/como-saber-si-un-elemento-esta-en-el-viewport
